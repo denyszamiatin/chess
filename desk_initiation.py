c43de9bd856810e88e7b43b28120261650aba628
Bohdan Pidopryhora <bogdanpodoprigora@gmail.com>
2015-09-30 18:55:34 +0300

    Merge branch 'master' of D:\PROGRAMMING\chess1 with conflicts.

--- desk_initiation.py
+++ desk_initiation.py
@@ -5,5 +5,7 @@ Initiation of chess desk 8x8
 with 0's in every checkerboard
 """
 
-def desk_initiation():
-    desk = [[x * 0 for x in xrange(8)] for y in xrange(8)]
+
+desk = [[x * 0 for x in xrange(8)] for y in xrange(8)]
+
+print desk
