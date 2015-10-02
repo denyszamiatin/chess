b38df90b12748c767f07b20b0bb49f80df95f082
Nativeborn <nativeborncitizen@gmail.com>
2015-10-02 19:14:03 +0300

    Code review #2

--- chessboard.py
+++ chessboard.py
@@ -2,6 +2,8 @@ __author__ = 'BohdanPidopryhora'
 
 BOARD_SIZE = 8
 EMPTY_CELL = 0
+VERTICAL_NAMES = '87654321'
+HORIZONTAL_NAMES = 'abcdefgh'
 
 
 def init_board():
@@ -12,13 +14,11 @@ def init_board():
 
 
 def convert_indexes_to_coords(row, column):
-    """this function convert index -1
+    """Converts index
     >>> convert_indexes_to_coords(7, 0)
     'a1'
     >>> convert_indexes_to_coords(0, 7)
     'h8'
     """
-    VERTICAL_NAMES = '87654321'
-    HORIZONTAL_NAMES = 'abcdefgh'
     return HORIZONTAL_NAMES[column] + VERTICAL_NAMES[row]
 
