f8121a2e15d51ef76d5405c5918c0f8cc41581e7
rosto <tvisvenya@gmail.com>
2015-10-02 19:15:12 +0300

    Merge branch 'master' of /home/rosto/PycharmProjects/chess with conflicts.

--- chessboard.py
+++ chessboard.py
@@ -2,8 +2,6 @@ __author__ = 'BohdanPidopryhora'
 
 BOARD_SIZE = 8
 EMPTY_CELL = 0
-VERTICAL_NAMES = '87654321'
-HORIZONTAL_NAMES = 'abcdefgh'
 
 
 def init_board():
@@ -14,11 +12,13 @@ def init_board():
 
 
 def convert_indexes_to_coords(row, column):
-    """Converts index
+    """this function convert index -1
     >>> convert_indexes_to_coords(7, 0)
     'a1'
     >>> convert_indexes_to_coords(0, 7)
     'h8'
     """
+    VERTICAL_NAMES = '87654321'
+    HORIZONTAL_NAMES = 'abcdefgh'
     return HORIZONTAL_NAMES[column] + VERTICAL_NAMES[row]
 
