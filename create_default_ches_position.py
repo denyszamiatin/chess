d099e45e30cc2164b3547a649a15a09200ccb90d
rosto <tvisvenya@gmail.com>
2015-10-06 18:48:40 +0300

    1 revision

--- create_default_ches_position.py
+++ create_default_ches_position.py
@@ -1,16 +1,6 @@
-__author__ = 'rosto'
-
-BOARD_SIZE = 8
-EMPTY_CELL = 0
-
-board = [[EMPTY_CELL for x in xrange(BOARD_SIZE)] for y in xrange(BOARD_SIZE)]
 
 
 def create_default_ches_position(board):
-    """
-
-    :type board: object
-    """
     board[7] = ['rook', 'knight', 'bishop', 'queen', 'king', 'bishop', 'knight', 'rook']
     board[0] = ['ROOK', 'KINGHT', 'BISHOP', 'QUEEN', 'KING', 'BISHOP', 'KNIGHT', 'ROOK']
     board[6] = ['pawns', 'pawns', 'pawns', 'pawns', 'pawns', 'pawns', 'pawns', 'pawns']
