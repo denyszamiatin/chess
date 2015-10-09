52dadf20b3df41a4154487d8d2b45e6b4160657e
rosto <tvisvenya@gmail.com>
2015-10-09 17:13:15 +0300

    Delete fail function check_gorizontal

--- chessboard.py
+++ chessboard.py
@@ -89,28 +89,15 @@ def check_pawn_move(start,dest):
            start[0] == dest[0] and start[1] - dest[1] == 0
 
 
-def check_gorizontal(coords):
-    """
-    Get horizontal figure
-    >>> check_gorizontal('a1')
-    ['R', 'K', 'B', 'Q', 'M', 'B', 'K', 'R']
-    >>> check_gorizontal('a2')
-    ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P']
-    >>> check_gorizontal('a6')
-    [0, 0, 0, 0, 0, 0, 0, 0]
-    """
-#   this is create test board
-    board = create_default_position(init_board())
-    return board[convert_coords_to_indexes(coords)[0]]
 
 def check_horizontal(coords):
     """
     Get horizontal figure
-    >>> check_gorizontal('a1')
+    >>> check_horizontal('a1')
     ['R', 'K', 'B', 'Q', 'M', 'B', 'K', 'R']
-    >>> check_gorizontal('a2')
+    >>> check_horizontal('a2')
     ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P']
-    >>> check_gorizontal('a6')
+    >>> check_horizontal('a6')
     [0, 0, 0, 0, 0, 0, 0, 0]
     """
 #   this is create test board
